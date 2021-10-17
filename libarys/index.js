8f22fff7250f43daf94431bd876f764c2184f910
B3YD4 <41930266+B3YD4@users.noreply.github.com>
2021-10-17 20:50:18 +0300

    Delete libarys directory

--- libarys/index.js
+++
@@ -1,5 +0,0 @@
-function videoAc() {
-	var embed = document.getElementById("url").value
-	var embed2 = embed.slice("-11");
-	document.getElementById('frame').src = "https://www.youtube.com/embed/" + embed2 + "?";
-}
